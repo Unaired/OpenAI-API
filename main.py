e878bda6dfb45f88830ec78cc485c3bdebb483fd
Unaired <danielghorbanian@gmail.com>
2023-08-30 07:26:55 -0400

    Working. Can tinker with system message.

--- main.py
+++ main.py
@@ -6,14 +6,35 @@ openai.api_key = "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"
 
 models = openai.Model.list()
 print(models.data[0].id)
-chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=[
-    {"role": "user", "content": "Hello worl"}
+
+messages=[
+
+    {"role": "system", "content": "You are a helpful assistant"},
+       
+]
+ 
+user_wants_to_continue = True
+
+while user_wants_to_continue == True:
+
+    user_input = input()
+
+    if user_input != "":
+        user_wants_to_continue = True
+
+    else: user_wants_to_continue = False
+
+    messages.append({"role": "user", "content": user_input})
+    chat_completion = openai.ChatCompletion.create(model="gpt-4", messages = messages)
+    assistant_response = chat_completion.choices[0].message.content
+    messages.append({"role": "assistant", "content": assistant_response})
+    print(assistant_response)
+
+
+
     
-])
 
 
 
 
-while True:
-    print(chat_completion.choices[0].message.content)
     
